3e3afe1a676c8de9cc4403619f1e409cb4f8ac83
AlexNoelHdz <alejandro.noel.hernandez@gmail.com>
2021-04-16 10:01:23 -0500

    Agregar archivos de proyecto.

---
+++ BlazorCastorDemoTripwire/Models/SquareModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCastorDemoTripwire.Models
+{
+    public class SquareModel
+    {
+        public int Id { get; set; }//Unique identifier for each square
+        public string Style { get; set; }//Style for each square, usefull to show/occult the square
+        private bool isShown;
+        public bool IsShown//Is Showing castor?
+        {
+            get => isShown;
+            set
+            {
+                isShown = value;
+                if (isShown)
+                {
+                    Style = "castor";
+                    Console.WriteLine($"Se cambió a castor {Id}");
+                }
+                else
+                {
+                    Style = "";
+                }
+            }
+        }
+    }
+}
---
+++ BlazorCastorDemoTripwire/Pages/BlazeCastor.cs
@@ -0,0 +1,72 @@
+﻿using BlazorCastorDemoTripwire.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorCastorDemoTripwire.Pages
+{
+    public partial class BlazeCastor
+    {
+        private int score = 0;//UserScore
+        private int currentTime = 60;//Time for user to play
+        int hitPosition = 0;
+        private string message = "";//Messages for user
+        bool gameIsRunning = true;//Indicate if game is executing or not
+
+        public List<SquareModel> Squares { get; set; } =
+            new List<SquareModel>();
+
+        public BlazeCastor()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                Squares.Add(new SquareModel
+                {
+                    Id = i
+                });
+            }
+        }
+
+        private void MouseUp(SquareModel s)
+        {
+            if (s.Id == hitPosition)
+            {
+                score += 1;
+            }
+        }
+        private void ChooseSquare()
+        {
+            foreach (var item in Squares)
+            {
+                item.IsShown = false;
+            }
+            var randomPosition = new Random().Next(0, 9);
+            Squares[randomPosition].IsShown = true;
+            Console.WriteLine(randomPosition);
+            hitPosition = randomPosition;
+            StateHasChanged();
+        }
+
+        private async Task GameLoop()
+        {
+            while (gameIsRunning)
+            {
+
+                currentTime--;
+                if (currentTime == 0)
+                {
+                    message = "EL JUEGO FINALIZÓ!";
+                    gameIsRunning = false;
+                }
+                ChooseSquare();
+                await Task.Delay(1000);
+            }
+        }
+
+        protected override async void OnInitialized()
+        {
+            await GameLoop();
+        }
+
+    }
+}
